2a1566f9deedd4797e44ecce416a62b00a9bbf1b
Håkon Mosvoll <hakon.mosvoll@payex.com>
2021-03-01 19:07:43 +0100

    Portal mvp

---
+++ js/portal.js
@@ -0,0 +1,30 @@
+(() => {
+    const portal = document.createElement('portal');
+    portal.src = 'http://localhost:8080/portal';
+    //portal.style = '...';
+
+    portal.classList.add('portal-transition');
+    portal.addEventListener('click', (evt) => {
+        // Animate the portal once user interacts
+        portal.classList.add('portal-reveal');
+    });
+
+    portal.addEventListener('transitionend', (evt) => {
+    if (evt.propertyName == 'transform') {
+        // Activate the portal once the transition has completed
+        portal.activate();
+    }
+    });
+
+    document.body.append(portal);
+
+    // When the user touches the preview (embedded portal):
+    // do fancy animation, e.g. expand …
+    // and finish by doing the actual transition
+    // setTimeout(() => {
+    //     portal.activate();
+    // }, 2000);
+})();
+
+
+
